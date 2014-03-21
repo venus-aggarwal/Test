5d46d00b52ee5e4e23ddf4d7ea61849f05c9f06a
Anurag Aggarwal <aanurag@avaya.com>
2014-03-21 13:54:01 +0530

    hello servlet

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	throws ServletException, IOException
+    {
+    	String name = req.getParameter("name");
+    	resp.getWriter().print("Hello " + name);
+    }
+}
